354170f7adcc93a328b515dfaff626a73b011fb2
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2025-12-04 02:13:38 +0530

    sanchay

--- src/components/Mainbanner.jsx
+++ src/components/Mainbanner.jsx
@@ -6,10 +6,10 @@ const Mainbanner = () => {
   return (
     <div className='relative'>
         <img src={banner} alt="banner" className='w-full hidden md:block' />
-          <img  src={banner} alt="banner" className='w-full  md:hidden' />
+          <img  src={assets.main_banner_bg_sm} alt="banner" className='w-full  md:hidden' />
 
-          <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
-            {/* <h1 className='text-3xl md:text-4xl lg:text-5xl font-blod text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 '>Grocery Mart -Selling Fresh Fruit and Vegetables.</h1> */}
+          <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24 '>
+            <h1 className='sm:hidden -mb-80 text-3xl  font-blod text-center  max-w-72  leading-tight  '>Grocery Mart -Selling Fresh Fruit and Vegetables.</h1>
       
           <div className='flex items-center mt-86 font-medium'>
            <Link to={"/products"} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white  cursor-pointer'>Shop Now
