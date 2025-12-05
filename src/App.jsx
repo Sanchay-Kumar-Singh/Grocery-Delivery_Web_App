d886c41eaa6b724bb285ec8d792ef7104630f365
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2025-12-05 11:24:48 +0530

    sanchay

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import ProductCategory from './pages/ProductCategory.jsx';
 import ProductDetails from './pages/ProductDetails.jsx';
 import Cart from './pages/Cart.jsx';
 import AddAddress from './pages/AddAddress.jsx';
- 
+import MyOrders from './pages/MyOrders.jsx';
+
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
   const { showUserLogin } = useAppContext();
@@ -26,10 +27,11 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
           <Route path='/contact' element={<Contact />} />
-          <Route path='/products/:category' element={<ProductCategory />} /> 
-          <Route path='/products/:category/:id' element={<ProductDetails/>} />
-          <Route path='cart' element={<Cart/>}></Route>
-          <Route path='/add-address' element={<AddAddress/>}></Route>
+          <Route path='/products/:category' element={<ProductCategory />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />}/>
+            <Route path='/my-orders' element={<MyOrders />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}</div>
